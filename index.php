7e011210418d57fc6e0195cbc0e3ab636759b92e
T1W3R <werquin.timothe@gmail.com>
2022-09-20 13:48:06 +0200

    index with header

--- index.php
+++ index.php
@@ -10,36 +10,43 @@
 </head>
 
 <body>
-    <?php 
-    function getConnexion(){
+    <?php
+    if (session_status() === PHP_SESSION_NONE) {
+        include "header_unconnected.php";
+    } else {
+        include "header_connected.php";
+    };
+
+    function getConnexion()
+    {
         $dsn = 'mysql:dbname=ap3;host=127.0.0.1:3308';
-      
+
         try {
             $bdd = new PDO($dsn, "root", "");
             return $bdd;
         } catch (PDOExeption $e) {
-            die('DB Error: '.$e->getMessage());
+            die('DB Error: ' . $e->getMessage());
         }
-      } 
-      
-        $sql = "SELECT pr_id, pr_nom, pr_coutHT FROM `produit`";
-        $bdd = getConnexion();
-        $query = $bdd->prepare($sql);
-        $query->execute();
-        $result = $query->fetchAll();
+    }
 
-        foreach($result as $res){
-            $getPhoto = "SELECT ph_chemin FROM photos WHERE fk_pr = ".$res['pr_id']."; ";
-            $queryGP = $bdd->prepare($getPhoto);
-            $queryGP->execute();
-            $resultGP = $queryGP->fetch();
-            echo "<img src='".$resultGP[0]."' width = '350px', height='350px'><br><p><a href='http://localhost/SLAM/AP3/AP3/produit.php?id=".$res['pr_id']."'>".$res['pr_nom']."</a> ".$res['pr_coutHT']."€ HT</p> ";
-        }
-        
+    $sql = "SELECT pr_id, pr_nom, pr_coutHT FROM `produit`";
+    $bdd = getConnexion();
+    $query = $bdd->prepare($sql);
+    $query->execute();
+    $result = $query->fetchAll();
 
-      
-      
-      ?>
+    foreach ($result as $res) {
+        $getPhoto = "SELECT ph_chemin FROM photos WHERE fk_pr = " . $res['pr_id'] . "; ";
+        $queryGP = $bdd->prepare($getPhoto);
+        $queryGP->execute();
+        $resultGP = $queryGP->fetch();
+        echo "<img src='" . $resultGP[0] . "' width = '350px', height='350px'><br><p><a href='http://localhost/SLAM/AP3/AP3/produit.php?id=" . $res['pr_id'] . "'>" . $res['pr_nom'] . "</a> " . $res['pr_coutHT'] . "€ HT</p> ";
+    }
+
+
+
+
+    ?>
 
 </body>
 
