cb85d70d2f9684a55678d6407f7311e1563af1eb
LucasLejeune <lucaslejeune0@gmail.com>
2022-10-04 16:55:56 +0200

    Page mon panier historique

---
+++ ValidationPanier.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+function construct_(){
+
+    $dsn = 'mysql:dbname=ap3;host=127.0.0.1:3308';
+
+    try {
+        $bdd = new PDO($dsn, "root", "");
+        return $bdd;
+    } catch (PDOExeption $e) {
+        die('DB Error: ' . $e->getMessage());
+    }
+}
+
+$updateStatus = "UPDATE `commande` SET `co_statut`='transmise' WHERE co_id =".$_GET["id"];
+$bdd = construct_();
+$queryUs = $bdd->prepare($updateStatus);
+$queryUs->execute();
+
+header('Location: http://localhost/SLAM/AP3/AP3/monPanier.php');
+
+
+
+
+?>
