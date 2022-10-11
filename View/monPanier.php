5837f1350ddce80eceed66421a4a202600f54003
LucasLejeune <lucaslejeune0@gmail.com>
2022-10-11 16:33:14 +0200

    Front mon panier

--- View/monPanier.php
+++ View/monPanier.php
@@ -35,24 +35,32 @@ if (sizeof($infosCo) >= 1) {
     echo "<center><h3>Votre panier est vide</h3></center>";
 }
 
-
+echo "<br><hr style='border: none; background-color: aliceblue; height:1px;'>
+<h2>Commandes précédentes</h2>";
 foreach ($resultGCo as $resGCo) {
-
-    echo "<div class='PanierTxt'>";
+    
+    if ($resultGCo[0] != $resGCo){
+        echo "<hr style='border: none; 
+        background-color: aliceblue; 
+        height:1px; 
+        margin-left: 300px; 
+        margin-right: 300px'>";
+    }
     //Afficher les commandes Passee
     $resultGCP = getCommandePassee($resGCo);
 
-    $texte = "<p>Commande numéro: " . $resultGCo[0]["co_id"] . " commandée le: " . $resultGCo[0]["co_date"] . "<br>Articles commandés: ";
+    $texte = "<div style='padding-left: 200px; 
+        padding-right: 200px; 
+        font-size:18px'>
+    <p><h3 style='text-align: center'><u>Commande numéro " . $resultGCo[0]["co_id"] . ": </h3>Commandée le:</u> " . $resultGCo[0]["co_date"] . "<br><u>Articles commandés:</u> <ul>";
     foreach ($resultGCP as $resGCP) {
-        $texte .= $resGCP["pr_nom"] . " (X" . $resGCP["pr_quantite"] . ") prix unitaire: " . $resGCP["pr_coutHT"] . " €<br>";
+        $texte .= "<li>".$resGCP["pr_nom"] . " (X" . $resGCP["pr_quantite"] . ") prix unitaire: " . $resGCP["pr_coutHT"] . " €</li>";
     }
-    $texte .= "Expédié depuis: " . $resultGCP[0]["ls_libelle"] . ". Adresse de livraison: " . $resultGCP[0]["cl_adresse"];
+    $texte .= "</ul><u>Expédié depuis:</u> " . $resultGCP[0]["ls_libelle"] . ".<br> <u>Adresse de livraison:</u> " . $resultGCP[0]["cl_adresse"];
     $prixTotal = $resultGCP[0]["co_prixTotal"] * 1.20 + 5;
-    $texte .= "<br>Prix HT: ".$resultGCP[0]["co_prixTotal"]. " +20% TVA : ".number_format((float)$resultGCP[0]["co_prixTotal"] * 1.20, 2, '.', '')."€ + 5€ frais de port: ".number_format((float)$prixTotal, 2 , '.','')."€ </p>";
-    echo $texte;
-    echo "</div>";
+    $texte .= "<br><u>Prix HT:</u> ".$resultGCP[0]["co_prixTotal"]. "€ <br><span style='font-size:15px; font-style:italic'>&emsp;+ 20 % TVA <br>&emsp;+ 5€ frais de port</span><br> <u>Prix TTC:</u> ".number_format((float)$prixTotal, 2 , '.','')."€ </p>";
+    echo $texte."</div>";
 }
-
 
 ?>
 <br>
