23c58173c129e81b515978d92a01a1c9176e25f6
LucasLejeune <lucaslejeune0@gmail.com>
2023-01-03 16:54:56 +0100

    correction encode

--- API/getArticle.php
+++ API/getArticle.php
@@ -2,11 +2,11 @@
 
 include ("../Model/api.php");
 
-echo json_encode(getArticle($_GET["id"]));
-echo json_encode(array("lieux" => getLieuArticle($_GET["id"])));
 $chemin = getPhotos($_GET["id"])[0];
 $chemin = str_replace("..","",$chemin);
-echo json_encode(array("photo" => "/SLAM/AP3/AP3".$chemin));
+$tab = array(getArticle($_GET["id"]), "photo" => "/SLAM/AP3/AP3".$chemin, array("lieux" => getLieuArticle($_GET["id"])));
+echo json_encode($tab);
+
 
 
  ?>
